7f2ac9a7a8a43a7acebb47e38abdfa5b278993f9
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-08-16 13:48:38 +0430

    Add a test for creating a launch

--- server/src/routes/launches/launches.test.js
+++ server/src/routes/launches/launches.test.js
@@ -10,3 +10,22 @@ describe("Getting launches", () => {
     // expect(response.status).toBe(200);
   });
 });
+
+describe("Creating launches", () => {
+  it("should create a launch object and return 201 Created status code", async () => {
+    const validLaunch = {
+      mission: "a new mission",
+      target: "somewhere far far away",
+      rocket: "good one",
+      launchDate: "January 4, 2028",
+    };
+    const response = await request(app)
+      .post("/launches")
+      .send(validLaunch)
+      .expect(201);
+    expect(response.body).toMatchObject({
+      ...validLaunch,
+      launchDate: new Date(validLaunch.launchDate).toISOString(),
+    });
+  });
+});
