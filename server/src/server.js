c166afc7b27c01c355a92f629e4b0bb3fd269c98
Mustafa Hayati <mustafa.hayati.webdev@gmail.com>
2022-08-18 16:20:11 +0430

    Install mongoose and connect to the local mongodb service.

--- server/src/server.js
+++ server/src/server.js
@@ -1,11 +1,27 @@
 const http = require("http");
+const mongoose = require("mongoose");
 const app = require("./app");
 const { loadPlanetsData } = require("./models/planets.model");
 
 const PORT = process.env.PORT || 8000;
+const MONGO_URL = "mongodb://127.0.0.1:27017/";
+const DB_NAME = "nasa";
+
 const server = http.createServer(app);
 
+mongoose.connection.once("open", () =>
+  console.log("✅ MongoDB Connection is Ready!")
+);
+
+mongoose.connection.on("error", (err) => {
+  console.error(`❌ ${err}`);
+});
+
 async function startServer() {
+  mongoose.connect(MONGO_URL + DB_NAME, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  });
   await loadPlanetsData();
 
   server.listen(PORT, () => {
